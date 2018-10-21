5dcc2a16766cedc5e441c42bd8f8744d21b90944
Marcelo Ubilla <marce.ubi@hotmail.com>
2018-10-21 09:32:17 -0300

    modificacion crearUsuario

---
+++ cibus/frontend/src/main/java/labtic/ui/CreateUserController.java
@@ -0,0 +1,32 @@
+package labtic.ui;
+
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import lombok.Data;
+import org.springframework.stereotype.Controller;
+
+
+
+@Data
+@Controller
+public class CreateUserController {
+
+
+
+
+    @FXML
+    private JFXTextField nameText;
+
+    @FXML
+    void guardarDatos(ActionEvent event) {
+        System.out.println("holi");
+        System.out.println(nameText.getText());
+
+
+
+    }
+
+
+}
